b785da28b435989e6ad85f08b9926ba4164844da
33R25V04Q6HX2ZR\Administrator <1376363776@qq.com>
2019-11-13 17:11:59 +0800

    elem2.0

---
+++ elem/src/kangyu/login.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+export class login extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             
+        }
+    }
+    
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default login
