320f024ed9393f30f6213b826490fa7f6ae49225
33R25V04Q6HX2ZR\Administrator <1376363776@qq.com>
2019-11-14 20:46:54 +0800

    4.0

--- elem/src/App.js
+++ elem/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
-
+import Chengshiliebiao from "./jiahan/chengshiliebiao"
+import Search from "./jiahan/search"
+import {Route,BrowserRouter} from "react-router-dom"
 function App() {
   return (
     <div className="App">
-      123
+     <BrowserRouter>
+      <Route exact path="/" component={Chengshiliebiao}></Route>
+      <Route path="/search/:id" component={Search}></Route>
+      </BrowserRouter>
     </div>
   );
 }
